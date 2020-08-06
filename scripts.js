e97918fda62399c3435639ab8f1cd895707c0620
trekgit <earnme.com@gmail.com>
2020-08-06 21:54:21 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -108,7 +108,7 @@ function resizeWindow() {
 			$('#splashPad').show();
 			//$('#splash .logo').show();
 			$('#splash').css({
-			  'position' : 'absolute',
+			  'position' : 'relative',
 			  'top' : '0',
 			  'height' : '100%',
 			});
